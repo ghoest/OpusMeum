e2585084cbfd55c60d8caf2d4041f57e82ce46f4
Jason Gose <ghoest@gmail.com>
2019-05-19 00:46:39 -0600

    Add files via upload

---
+++ src/date.tcl
@@ -0,0 +1,43 @@
+# vim: filetype=tcl shiftwidth=4 smarttab expandtab
+####################################################################################################
+# NAME       : rtag_date.tcl
+#
+# DESCRIPTION: Replacement Tag {date}
+#
+# PROCEDURES : date_rtag
+#
+# VARIABLES  : ::_known_rtag -- used in OpusMeum.tcl
+####################################################################################################
+lappend ::_known_rtag {date}
+
+################################################################################
+#   Procedure: date_rtag
+#
+# Description: return a date given the input...
+#
+# Parameters
+# ==========
+# input             I   input for procedure.
+#
+# The following forms of input are acceptable
+#     {"date:" $clkFmt $when}
+#     {"date:" $clkFmt}
+# $clkFmt can be anything accepted by 'clock format ... -format'
+# $when is anything accepted by 'clock scan'
+#
+# Return Value
+# ============
+# true/false
+#
+# Error Handling
+# ==============
+# None.
+################################################################################
+proc date_rtag {input} {
+    if {[llength $input] == 2} {
+        set when [lindex $input 1]
+    } else {
+        set when "now"
+    }
+    return [clock format [clock scan $when] -format [lindex $input 0]]
+}
